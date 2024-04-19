8a90661b227d533eb75f88b5887aa7f05e91f35a
Jimmy!! <132008245+kuroifantaisie@users.noreply.github.com>
2024-04-18 22:49:22 -0500

    Added Code to debug Board Type

--- FinalProjectCode/FinalProjectCode.py
+++ FinalProjectCode/FinalProjectCode.py
@@ -3,7 +3,8 @@ import random
 import board
 import adafruit_dotstar as dotstar
 
-dots = dotstar.DotStar(board.D6, board.D5, 30, brightness=0.2)
+print(dir(board))
+dots = dotstar.DotStar(board.SCK, board.MOSI, 30, brightness=0.2)
 
 def random_color():
     return random.randrange(0, 7) * 32
