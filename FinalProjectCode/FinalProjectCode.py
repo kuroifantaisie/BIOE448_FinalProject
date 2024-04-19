bec986379d7252491cd11003f5e5fc5206fc1e60
Jimmy!! <132008245+kuroifantaisie@users.noreply.github.com>
2024-04-19 00:24:42 -0500

    Commented out LED code, added in bluetooth code

--- FinalProjectCode/FinalProjectCode.py
+++ FinalProjectCode/FinalProjectCode.py
@@ -1,20 +1,37 @@
 import time
 import random
-from board import board
+import board
 import adafruit_dotstar as dotstar
+import bluetooth 
 
-print(dir(board))
-dots = dotstar.DotStar(board.SCLK, board.MOSI, 30, brightness=0.2)
+server_sock=bluetooth.BluetoothSocket(bluetooth.RFCOMM) 
+port = 22
+server_sock.bind(("",port)) 
+server_sock.listen(1) 
+client_sock,address = server_sock.accept() 
+print ("Connection made with: ", address) 
+while True: 
+    recvdata = client_sock.recv(1024) 
+    print ("Information Received: %s" % recvdata)
+    if (recvdata == "Q"): 
+        print ("Finished.") 
+        break
+         
+# client_sock.close() 
+# server_sock.close()
 
-def random_color():
-    return random.randrange(0, 7) * 32
+# print(dir(board))
+# dots = dotstar.DotStar(board.SCLK, board.MOSI, 30, brightness=0.2)
+
+# def random_color():
+#     return random.randrange(0, 7) * 32
 
 
-# MAIN LOOP
-n_dots = len(dots)
-while True:
-    # Fill each dot with a random color
-    for dot in range(n_dots):
-        dots[dot] = (random_color(), random_color(), random_color())
+# # MAIN LOOP
+# n_dots = len(dots)
+# while True:
+#     # Fill each dot with a random color
+#     for dot in range(n_dots):
+#         dots[dot] = (random_color(), random_color(), random_color())
 
-    time.sleep(0.25)
+#     time.sleep(0.25)
